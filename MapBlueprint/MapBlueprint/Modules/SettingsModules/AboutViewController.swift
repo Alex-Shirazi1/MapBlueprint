275270dd9205caf8b6c7dc2089dfc063d0b5f1ad
Alex Shirazi <alexshirazi@Shirazis-MacBook-Pro-2.local>
2024-02-20 00:43:23 -0800

    Alpha 1.3

--- MapBlueprint/MapBlueprint/Modules/SettingsModules/AboutViewController.swift
+++ MapBlueprint/MapBlueprint/Modules/SettingsModules/AboutViewController.swift
@@ -9,7 +9,7 @@ import Foundation
 import UIKit
 
 class AboutViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    let version = "Alpha 1.2.0" // TODO: use remote config to determine version
+    let version = "Alpha 1.3" // TODO: use remote config to determine version
     
     let tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
